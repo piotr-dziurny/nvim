cb42f1e0600a35409640910d0931b2e7205c5fca
piotr <aisujd1@gmail.com>
2025-01-04 18:15:24 +0100

    got rid of copilotchat as it was ultra useless

---
+++ lua/core/plugins/utils/copilot.lua
@@ -0,0 +1,50 @@
+return {
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        event = 'InsertEnter',
+        opts = {
+            panel = { enabled = false },
+            suggestion = {
+                auto_trigger = true,
+                -- Use Alt to interact with Copilot.
+                keymap = {
+                    -- Disable the built-in mapping, we'll configure it in nvim-cmp.
+                    accept = false,
+                    accept_word = '<M-w>',
+                    accept_line = '<M-l>',
+                    next = '<M-]>',
+                    prev = '<M-[>',
+                    dismiss = '/',
+                },
+            },
+            filetypes = { markdown = true },
+        },
+        config = function(_, opts)
+            local cmp = require 'cmp'
+            local copilot = require 'copilot.suggestion'
+
+            -- Set up Copilot with the provided options.
+            require('copilot').setup(opts)
+
+            -- Function to control suggestion visibility.
+            local function set_trigger(trigger)
+                vim.b.copilot_suggestion_auto_trigger = trigger
+                vim.b.copilot_suggestion_hidden = not trigger
+            end
+
+            -- Hide suggestions when the completion menu is open.
+            cmp.event:on('menu_opened', function()
+                if copilot.is_visible() then
+                    copilot.dismiss()
+                end
+                set_trigger(false)
+            end)
+
+            -- Re-enable suggestions when the menu closes.
+            cmp.event:on('menu_closed', function()
+                set_trigger(true) -- Always re-enable suggestions after menu closes.
+            end)
+        end,
+    },
+}
