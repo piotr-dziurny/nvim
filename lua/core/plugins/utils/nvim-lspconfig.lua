5c51228eb3ddcf7526c3298e62a383459900b72a
piotr <aisujd1@gmail.com>
2025-09-07 23:40:18 +0200

    fixed pyright not recognizing pyenv

--- lua/core/plugins/utils/nvim-lspconfig.lua
+++ lua/core/plugins/utils/nvim-lspconfig.lua
@@ -3,19 +3,36 @@ return {
   config = function()
     local lspconfig = require("lspconfig")
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
+   
+    -- pyenv venv support
+    local function get_python_path()
+      local python_version_file = vim.fn.getcwd() .. "/.python-version"
+      if vim.fn.filereadable(python_version_file) == 1 then
+        local version = vim.fn.readfile(python_version_file)[1]
+        local python_path = vim.fn.expand("~/.pyenv/versions/" .. version .. "/bin/python")
+        if vim.fn.executable(python_path) == 1 then
+          return python_path
+        end
+      end
+      return nil -- pyright will detect python's standard venv from the shell (if activated)
+    end
 
     -- python
     lspconfig.pyright.setup({
       capabilities = capabilities,
       settings = {
         python = {
+          pythonPath = get_python_path(),
           analysis = {
-            typeCheckingMode = "off",
+            typeCheckingMode = "on",
           }
         }
-      }
+      },
+      on_new_config = function(new_config, new_root_dir)
+        new_config.settings.python.pythonPath = get_python_path()
+      end,
     })
-
+    
     -- go
     lspconfig.gopls.setup({
       capabilities = capabilities,
@@ -31,3 +48,4 @@ return {
     })
   end,
 }
+
--- lua/core/plugins/utils/telescope.lua
+++ lua/core/plugins/utils/telescope.lua
@@ -19,10 +19,14 @@ return {
      },
    })
    
-   local builtin = require("telescope.builtin")
-   vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-   vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-   vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-   vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+  local builtin = require("telescope.builtin")
+  vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+  vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+  vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+  vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+  vim.keymap.set("n", "<leader>dd", builtin.diagnostics, { desc = "Telescope diagnostics (project)" })
+  vim.keymap.set("n", "<leader>db", function()
+    builtin.diagnostics({ bufnr = 0 })
+  end, { desc = "Telescope diagnostics (buffer)" })
  end,
 }
